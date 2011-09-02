845221fda37b06e1f6fbd79ddd5436e968efba7e
Butort Haliam <butorthaliam@hotmail.com>
2011-09-02 08:51:37 +0800

    fix bugs

--- algorithm/src/com/github/esdaprojects/math/lIIIIlIllIIlIIlI.java
+++ algorithm/src/com/github/esdaprojects/math/lIIIIlIllIIlIIlI.java
@@ -9,7 +9,65 @@ public class lIIIIlIllIIlIIlI
   private static final String _$2 = "abcdefghijklmnopqrstuvwxyz018967";
   private static final int[] _$1 = { 255, 255, 26, 27, 28, 29, 30, 31, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255 };
   
-  public static String _$1(String paramString, int paramInt1, int paramInt2)
+  public static byte[] _$1(String paramString)
+  {
+    String str = paramString + "";
+    for (int i = 0; i < 32; i++) {
+      str = str.replaceAll("abcdefghijklmnopqrstuvwxyz018967".charAt(i) + "", "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".charAt(i) + "");
+    }
+    byte[] arrayOfByte = new byte[str.length() * 5 / 8];
+    int i = 0;
+    int j = 0;
+    int m = 0;
+    while (i < str.length())
+    {
+      int k = str.charAt(i) - '0';
+      if ((k >= 0) && (k < _$1.length))
+      {
+        int n = _$1[k];
+        if (n != 255) {
+          if (j <= 3)
+          {
+            j = (j + 5) % 8;
+            if (j == 0)
+            {
+              int tmp184_182 = m;
+              byte[] tmp184_180 = arrayOfByte;
+              tmp184_180[tmp184_182] = ((byte)(tmp184_180[tmp184_182] | n));
+              m++;
+              if (m >= arrayOfByte.length) {
+                break;
+              }
+            }
+            else
+            {
+              int tmp209_207 = m;
+              byte[] tmp209_205 = arrayOfByte;
+              tmp209_205[tmp209_207] = ((byte)(tmp209_205[tmp209_207] | n << 8 - j));
+            }
+          }
+          else
+          {
+            j = (j + 5) % 8;
+            int tmp235_233 = m;
+            byte[] tmp235_231 = arrayOfByte;
+            tmp235_231[tmp235_233] = ((byte)(tmp235_231[tmp235_233] | n >>> j));
+            m++;
+            if (m >= arrayOfByte.length) {
+              break;
+            }
+            int tmp262_260 = m;
+            byte[] tmp262_258 = arrayOfByte;
+            tmp262_258[tmp262_260] = ((byte)(tmp262_258[tmp262_260] | n << 8 - j));
+          }
+        }
+      }
+      i++;
+    }
+    return arrayOfByte;
+  }
+
+public static String _$1(String paramString, int paramInt1, int paramInt2)
   {
     if (paramString == null) {
       return null;
@@ -71,68 +129,10 @@ public class lIIIIlIllIIlIIlI
     return localStringBuffer1.toString();
   }
   
-  public static byte[] _$1(String paramString)
-  {
-    String str = paramString + "";
-    for (int i = 0; i < 32; i++) {
-      str = str.replaceAll("abcdefghijklmnopqrstuvwxyz018967".charAt(i) + "", "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".charAt(i) + "");
-    }
-    byte[] arrayOfByte = new byte[str.length() * 5 / 8];
-    int i = 0;
-    int j = 0;
-    int m = 0;
-    while (i < str.length())
-    {
-      int k = str.charAt(i) - '0';
-      if ((k >= 0) && (k < _$1.length))
-      {
-        int n = _$1[k];
-        if (n != 255) {
-          if (j <= 3)
-          {
-            j = (j + 5) % 8;
-            if (j == 0)
-            {
-              int tmp184_182 = m;
-              byte[] tmp184_180 = arrayOfByte;
-              tmp184_180[tmp184_182] = ((byte)(tmp184_180[tmp184_182] | n));
-              m++;
-              if (m >= arrayOfByte.length) {
-                break;
-              }
-            }
-            else
-            {
-              int tmp209_207 = m;
-              byte[] tmp209_205 = arrayOfByte;
-              tmp209_205[tmp209_207] = ((byte)(tmp209_205[tmp209_207] | n << 8 - j));
-            }
-          }
-          else
-          {
-            j = (j + 5) % 8;
-            int tmp235_233 = m;
-            byte[] tmp235_231 = arrayOfByte;
-            tmp235_231[tmp235_233] = ((byte)(tmp235_231[tmp235_233] | n >>> j));
-            m++;
-            if (m >= arrayOfByte.length) {
-              break;
-            }
-            int tmp262_260 = m;
-            byte[] tmp262_258 = arrayOfByte;
-            tmp262_258[tmp262_260] = ((byte)(tmp262_258[tmp262_260] | n << 8 - j));
-          }
-        }
-      }
-      i++;
-    }
-    return arrayOfByte;
-  }
-  
   public static void main(String[] paramArrayOfString)
     throws UnsupportedEncodingException
   {
-    String str1 = "hello，USA! yu7654wedfgnhjmki78u6y54redfdv";
+    String str1 = "hello, USA! yu7654wedfgnhjmki78u6y54redfdv";
     String str2 = _$1(str1, 256, 62);
     System.out.println(str2);
     System.out.println(new String(_$1(str2), "utf8"));
