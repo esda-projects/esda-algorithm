974c631a482e88297dbe0f7e20bf2f84cfdfbc87
butorthaliam <butorthaliam@hotmail.com>
2014-07-02 17:22:47 +0800

    utils

---
+++ algorithm/src/com/github/esdaprojects/security/lIIlIlIIlIlllIll.java
@@ -0,0 +1,18 @@
+package com.github.esdaprojects.security;
+
+public class lIIlIlIIlIlllIll
+{
+  public static IlIIlIlIllllIlIl _$1(String paramString)
+  {
+    if (paramString == null) {
+      return null;
+    }
+    if (paramString.toLowerCase().equals("esda")) {
+      return new IIlIlllIIlIIIllI();
+    }
+    if (paramString.toLowerCase().equals("esda32")) {
+      return new IlIllIllIIIIIllI();
+    }
+    return null;
+  }
+}
